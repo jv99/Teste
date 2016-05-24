b6a1c2f6b5a8d04b8c50f3c1697db3e2cc47a270
jv99 <d42c01a6@mohmal.in>
2016-05-24 15:11:40 -0300

    Add files via upload

---
+++ teste1.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <conio.h>
+#include <locale.h>
+
+main (){
+	setlocale(LC_ALL, "Portuguese");
+	
+	float v1,v2,result;
+	int op;
+	
+	printf ("Informe um valor para v1: \n");
+		scanf ("%f",&v1);
+	
+	printf ("Informe um valor para v2: \n");
+		scanf ("%f",&v2);
+	
+	printf ("Informe 1 para soma | Informe 2 para subtração \n");
+		scanf ("%i",&op);
+		
+		if (op == 1) {
+			result = v1+v2;
+			printf ("Resultado da soma: %.2f",result);
+			
+		}
+		
+		if (op == 2) {
+			
+				if (v1>v2){
+					result = (v1-v2);
+					printf ("Resultado da subtração: %.2f",result);
+				}
+				else {
+					result = (v2-v1);
+					printf ("Resultado da subtração: %.2f",result);
+				}
+
+		}
+	
+		
+}
